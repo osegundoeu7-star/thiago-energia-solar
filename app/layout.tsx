b83c9e67ad97a28c5aa6e4c4bb69552cbf766ea5
osegundoeu7-star <osegundoeu7@gmail.com>
2026-08-10 17:42:56 -0300

    correção nome

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Energia Solar em Rondônia e Mato Grosso | Simões Energia Solar',
-  description: 'Simule sua economia com energia solar e solicite uma análise gratuita para residências, comércios e propriedades em Rondônia e Mato Grosso.',
+  description: 'Simule sua economia com energia solar e solicite uma análise gratuita para residências, comércios e propriedades',
   verification: {
     google: 'UZpq3HE-0hM__7mgAJWtY8OPgmXHIf0laB7nb6bJBIU'
   }
